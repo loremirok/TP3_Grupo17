9e57d2864ecd75ae088de181220530c985bff0ae
User <User@DESKTOP-85PBKTA>
2022-04-21 22:11:34 -0300

    creando arreglos

--- TP3_Grupo17/src/main/java/ar/edu/unju/fi/ejercicio1/Principal.java
+++ TP3_Grupo17/src/main/java/ar/edu/unju/fi/ejercicio1/Principal.java
@@ -1,9 +1,15 @@
 package ar.edu.unju.fi.ejercicio1;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner lectura = new Scanner(System.in);
+		int[] arregloDeNumero1 = new int[5];
+		int[] arregloDeNumero2 = new int[5];
+		boolean continua;
 
 	}
 
