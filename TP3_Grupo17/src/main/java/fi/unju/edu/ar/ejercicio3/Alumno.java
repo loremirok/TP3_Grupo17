9a2d04b91f6a5c531dd2417cfcd5143949349e6e
User <User@DESKTOP-85PBKTA>
2022-04-23 12:55:02 -0300

    Agregando el ejercicio 3, promedio y nota maxima de un alumno

---
+++ TP3_Grupo17/src/main/java/fi/unju/edu/ar/ejercicio3/Alumno.java
@@ -0,0 +1,86 @@
+package fi.unju.edu.ar.ejercicio3;
+
+public class Alumno {
+	private String nombre;
+	private String apellido;
+	private int libretaUniversitaria;
+	private double[] nota=new double[5];
+	
+	public Alumno() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Alumno(String nombre, String apellido, int libretaUniversitaria, double[] nota) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.libretaUniversitaria = libretaUniversitaria;
+		this.nota = nota;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getLibretaUniversitaria() {
+		return libretaUniversitaria;
+	}
+
+	public void setLibretaUniversitaria(int libretaUniversitaria) {
+		this.libretaUniversitaria = libretaUniversitaria;
+	}
+
+	public double[] getNota() {
+		return nota;
+	}
+
+	public void setNota(double[] nota) {
+		this.nota = nota;
+	}
+
+	
+	public void agregarNota(double calificacion, int posicion) {
+        nota[posicion] = calificacion;
+    }
+	
+	public double calcularPromedio() {
+		double promedio,suma=0;
+		int i;
+		for ( i=0; i<nota.length;i++) {
+			suma=suma+nota[i];
+		}
+		promedio=suma/i;
+		return promedio;
+	}
+	
+	public double obtenerNotaMaxima() {
+		double notaMax=0;
+		int i;
+		boolean band=true;
+		for(i=0;i<nota.length;i++) {
+			if(band) {
+				notaMax=nota[i];
+				band=false;
+			}else {
+				if(nota[i]>notaMax) {
+					notaMax=nota[i];
+				}
+			}
+		}
+		return notaMax;
+	}
+	
+	
+}
---
+++ TP3_Grupo17/src/main/java/fi/unju/edu/ar/ejercicio3/Principal.java
@@ -0,0 +1,72 @@
+package fi.unju.edu.ar.ejercicio3;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner lectura = new Scanner(System.in);
+		Alumno unAlumno = new Alumno();
+
+		System.out.print("ingrese nombre de alumno: ");
+		String nombre = lectura.nextLine();
+		unAlumno.setNombre(nombre);
+
+		System.out.print("ingrese apellido de alumno: ");
+		String apellido = lectura.nextLine();
+		unAlumno.setApellido(apellido);
+
+		boolean continua;
+		do {
+			try {
+				continua = false;
+				System.out.print("ingrese libreta universitaria de alumno: ");
+				int lu = lectura.nextInt();
+				unAlumno.setLibretaUniversitaria(lu);
+			} catch (InputMismatchException ex) {
+				System.out.println("Debe ingresar un número entero");
+				lectura.nextLine();
+				continua = true;
+			}
+		} while (continua);
+
+		System.out.println("ingrese 5 notas de alumno: ");
+		double nota;
+		for (int i = 0; i < unAlumno.getNota().length; i++) {
+			do {
+				try {
+					continua = false;
+					System.out.print("ingrese nota para indice " + i + ": ");
+					nota = lectura.nextDouble();
+					if ((nota >= 0) && (nota <= 10)) {
+						unAlumno.agregarNota(nota, i);
+					} else {
+						System.out.println("debe ingresar un numero entre [0,10]");
+						lectura.nextLine();
+						continua = true;
+					}	
+				} catch (InputMismatchException ex) {
+					System.out.println("Debe ingresar un número entero");
+					lectura.nextLine();
+					continua = true;
+				}
+			} while (continua);
+		}
+
+		System.out.println("nombre de alumno: "+unAlumno.getNombre());
+		System.out.println("apellido de alumno: "+unAlumno.getApellido());
+		System.out.println("libreta universitaria de alumno: "+unAlumno.getLibretaUniversitaria());
+		System.out.print("notas del alumno: ");
+		for (int i = 0; i < unAlumno.getNota().length; i++) {
+			System.out.print(unAlumno.getNota()[i]+"  ");
+		}
+		lectura.close();
+		System.out.println();
+		System.out.println("el promedio de las notas del alumno es: " + unAlumno.calcularPromedio());
+		System.out.println("la nota máxima del alumno es: " + unAlumno.obtenerNotaMaxima());
+
+	}
+
+}
